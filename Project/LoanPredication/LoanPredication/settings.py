acd6a3615e05bab2f7db4ca8d808a8cabaf01481
dafalsanika30 <149603857+dafalsanika30@users.noreply.github.com>
2025-10-29 22:54:42 +0530

    Update settings.py

--- Project/LoanPredication/LoanPredication/settings.py
+++ Project/LoanPredication/LoanPredication/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django-bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +129,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
